42bece599e8245628e2e85cab154d69da281b1ae
yonchi <2yonchi@gmail.com>
2021-03-06 21:11:48 +0200

    feat: added phpUnit config

---
+++ tests/Unit/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace EmailTypePrediction\EmailTypePrediction\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testFirst():void
+    {
+        $this->assertTrue(true);
+    }
+}
