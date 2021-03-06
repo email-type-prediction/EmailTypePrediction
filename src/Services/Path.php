787c0ce9768b42dc99acb54d12e7bdc3b5093cb7
Yonchi <2yonchi@gmail.com>
2021-03-06 17:26:17 +0200

    Update src/Services/Path.php
    Co-authored-by: Yozhef <hisemjo@gmail.com>

--- src/Services/Path.php
+++ src/Services/Path.php
@@ -53,7 +53,9 @@ class Path
      */
     private function validate(?string $path): void
     {
-        if (is_null($path)) {
+        if (is_null($path) || in_array($path, self::values(), true) === false) {
+        throw new InvalidPathException();
+        }
             throw new InvalidPathException();
         }
         if (!in_array($path, self::values(), true)){
